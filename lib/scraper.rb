790abccf5306593454c798ae119e8b3c18fb696f
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2020-01-03 21:14:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,7 +13,7 @@ class Scraper
 
       students = {}
 
-      kickstarter.css("li.project.grid_4").each do |student|
+      doc.css("li.project.grid_4").each do |student|
       name = student.css(".student-cart.card-text-container h4.student-name").text
       students[name.to_sym] = {
         :location => student.css(".student-cart.card-text-container p.student-location").text,
@@ -27,7 +27,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
   #  Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
 
-  #  kickstarter.css("li.project.grid_4").each do |student|
+  #  doc.css("li.project.grid_4").each do |student|
   #  title = student.css("h2.bbcard_name strong a").text
   #  student[title.to_sym] = {
   #    :twitter => student.css("div.project-thumbnail a img").attribute("src").value,
